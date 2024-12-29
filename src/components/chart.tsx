039979d33dc0479ea3421e7c529246c70df4e6c4
Shey Laplanche <sheylaplanche@gmail.com>
2024-12-28 20:00:27 -0800

    Updated fp display fp chart

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -98,12 +98,14 @@ function PlayerTooltip({ active, payload, label }: PlayerTooltipProps) {
 	if (active && payload) {
 		const player = payload[0].payload;
 		return (
-			<div className="custom-tooltip bg-card text-card-foreground shadow rounded border flex items-end justify-between gap-4">
-				<PlayerHeadshot
-					playerId={String(player.id)}
-					size={96}
-					className="p"
-				/>
+			<div className="custom-tooltip bg-card text-card-foreground shadow rounded border flex items-center justify-between gap-2">
+				<div className="ml-1 border-b ">
+					<PlayerHeadshot
+						playerId={String(player.id)}
+						size={96}
+						className="p"
+					/>
+				</div>
 				<div className="p-2">
 					<p className="name text-lg mb-2 font-semibold flex items-center gap-1">
 						<span>
@@ -113,26 +115,33 @@ function PlayerTooltip({ active, payload, label }: PlayerTooltipProps) {
 							vs. {player.opposingTeam.name} {label}
 						</span>
 					</p>
-					<div className="grid grid-cols-8 text-center text-muted-foreground text-xs">
+					<div className="grid grid-cols-6 text-center text-muted-foreground text-xs">
 						<div className="p-0.5 font-bold">PTS</div>
 						<div className="p-0.5 font-bold">REB</div>
 						<div className="p-0.5 font-bold">AST</div>
 						<div className="p-0.5 font-bold">STL</div>
 						<div className="p-0.5 font-bold">BLK</div>
 						<div className="p-0.5 font-bold">TOV</div>
-						<div className="p-0.5 font-bold">FP</div>
-						<div className="p-0.5 font-bold">&delta;</div>
 						<div className="p-0.5">{player.points}</div>
 						<div className="p-0.5">{player.reboundsTotal}</div>
 						<div className="p-0.5l">{player.assists}</div>
 						<div className="p-0.5l">{player.steals}</div>
 						<div className="p-0.5l">{player.blocks}</div>
 						<div className="p-0.5l">{player.turnovers}</div>
-						<div className="p-0.5">{player.fp}</div>
-						<div className="p-0.5">
-							{player.fpDelta > 0 ? "+" : ""}
-							{player.fpDelta}
-						</div>
+					</div>
+				</div>
+				<div className="flex flex-col items-center justify-center min-h-full p-2 aspect-square">
+					<p className="text-xs text-muted-foreground">FP</p>
+					<div className="p-0.5 text-xl font-semibold">
+						{player.fp}
+					</div>
+					<div
+						className={`text-sm px-1 rounded ${
+							player.fpDelta > 0 ? "bg-green-500" : "bg-red-500"
+						}`}
+					>
+						{player.fpDelta > 0 ? "+" : ""}
+						{player.fpDelta}
 					</div>
 				</div>
 			</div>
